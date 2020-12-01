facca42d06542abb794dbc7a40a7e72fbe36c32b
Marian Dumitru <d.mariandev@gmail.com>
2020-12-01 22:50:09 +0200

    class signup service - handles all class sign up logic

--- app/Services/ClassSignupService.php
+++ app/Services/ClassSignupService.php
@@ -5,38 +5,39 @@ namespace App\Services;
 use App\Repositories\ClassesRepository;
 use App\Repositories\ClassStudentRepository;
 use App\Repositories\MainClassRepository;
+use App\Repositories\ContentRepository;
 
 class ClassSignupService
 {
     protected $mainClassRepository;
     protected $classesRepository;
-    private $classStudentRepository;
+    protected $classStudentRepository;
+    protected   $contentRepository;
 
-    public function __construct(MainClassRepository $mainClassRepository, ClassesRepository $classesRepository, ClassStudentRepository $classStudentRepository)
+    public function __construct(MainClassRepository $mainClassRepository, ClassesRepository $classesRepository, ClassStudentRepository $classStudentRepository, ContentRepository $contentRepository)
     {
         $this->mainClassRepository = $mainClassRepository;
         $this->classesRepository = $classesRepository;
         $this->classStudentRepository = $classStudentRepository;
+        $this->contentRepository = $contentRepository;
     }
 
-    public function getSignupData(int $mainClassId, int $authId, int $classId, string $pageType)
+    public function getSignupData(int $mainClassId, int $studentId, int $classId, string $pageType)
     {
-
         $mainClasses = $this->mainClassRepository->getOrdered();
         $mainClass = $this->mainClassRepository->getById($mainClassId);
-        $classesByStartDate = $this->classesRepository->orderByStartDate($mainClassId);
-        $studentClassesCount = $this->classStudentRepository->countStudentClasses($authId, $classId);
-        $signedUp = false;
 
-        if ($studentClassesCount > 0) {
-            $signedUp = true;
-        }
+        $classesByStartDate = $this->classesRepository->orderByStartDate($mainClassId);
+
+        $student = $this->classStudentRepository->getClassStudentData($studentId, $classId);
+        $signedUp = $student->count() > 0;
 
         $termsOfService = $this->contentRepository->getContentData($pageType);
 
         $signupData = [];
-        $signupData['$mainClasses'] = $mainClasses;
-        $signupData['$mainClass'] = $mainClass;
+        $signupData['student'] = $student;
+        $signupData['mainClasses'] = $mainClasses;
+        $signupData['mainClass'] = $mainClass;
         $signupData['classesByStartDate'] = $classesByStartDate;
         $signupData['signedUp'] = $signedUp;
         $signupData['termsOfService'] = $termsOfService;
