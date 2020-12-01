fa516f10317b69cc050cf75b7a4d66c344f6c5ad
Marian Dumitru <d.mariandev@gmail.com>
2020-12-01 22:45:41 +0200

    hardcoded arguments in controller's methods. Can't sign in with student role = ca't get url params values

--- app/Http/Controllers/ClassSignupController.php
+++ app/Http/Controllers/ClassSignupController.php
@@ -3,35 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Constants\ContentConstants;
-use App\Models\MainClass;
 use App\Services\ClassSignupService;
 use Illuminate\Http\Request;
 
-class ClassSignupController
+class ClassSignupController extends Controller
 {
     protected $classSignupService;
-    protected $request;
 
     public function __construct(
-        ClassSignupService $classSignupService,
-        Request $request
+        ClassSignupService $classSignupService
     ) {
         $this->classSignupService = $classSignupService;
-        $this->request = $request;
     }
 
-    /**
-     * @param Request $request
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-     * @throws \Exception
-     */
-    protected function __invoke(Request $request, MainClass $mainClass)
+    protected function classSignup (Request $request)
     {
-        $authId = $request->user()->id;
-        $mainClassId = $this->request->input('id');
-        $classId = $this->request->input('id');
-
-        $signupData = $this->classSignupService->getSignupData($request->input('main_class_id'), $request->user()->id, $request->input('class_id'), ContentConstants::TERMS_OF_SERVICE);
+        $signupData = $this->classSignupService->getSignupData(1, 53, 39, ContentConstants::TERMS_OF_SERVICE);
 
         return view('class_signup_form')->with([
             'signupData' => $signupData,
