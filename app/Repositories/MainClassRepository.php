170fb800c10c8c7b396e2f45adf3391d35f9969c
Marian Dumitru <d.mariandev@gmail.com>
2020-12-09 00:03:30 +0200

    removed get mainclasses ordered and get by id methods  from main class repo

--- app/Repositories/MainClassRepository.php
+++ app/Repositories/MainClassRepository.php
@@ -102,18 +102,6 @@ class MainClassRepository extends Repository
         return $model->get();
     }
 
-    public function getOrdered()
-    {
-        $mainClasses = $this->model::where('is_active', 1)->orderBy('order', 'asc')->get();
-
-        return $mainClasses;
-    }
-
-    public function getById(int $mainClassId) {
-        $mainClass = $this->model::with('classes')->findOrFail($mainClassId);
-
-        return $mainClass;
-    }
 }
 
 
