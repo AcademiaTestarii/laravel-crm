2ac1ff92c33e55f4749427ca8e1b477973b688c9
Marian Dumitru <d.mariandev@gmail.com>
2021-01-10 01:07:42 +0200

    top menu refactor

--- resources/views/include/temp_includes/top_menu.blade.php
+++ resources/views/include/temp_includes/top_menu.blade.php
@@ -1,61 +1,22 @@
-<div class="header-nav">
-  <div class="header-nav-wrapper navbar-scrolltofixed">
-    <div class="container">
-      <nav id="menuzord-right" class="menuzord yellow ">
-        <a class="menuzord-brand pull-left flip" href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/index.php"><img src="images/logo-academia-testarii.png" alt=""></a>
-        <ul class="menuzord-menu">
+<li>
+  <a href="/"><i class="fa fa-th-large"></i> <span class="nav-label">AT</span></a>
+</li>
+<li @if (\Request::is('catalog')) class="active" @endif >
+  <a href="/catalog"><i class="fa fa-th-large"></i> <span class="nav-label">Catalog</span></a>
+</li>
+<li>
+  @if (Request::is('classes_offer')) class="active" @endif >
+  <a href="/classes_offer"><i class="fa fa-home"></i> <span class="nav-label">Cursuri pentru tine</span></a>
+</li>
 
-          <li <?php if ($page == "home") {
-                echo 'class="home active"> <a href="#up">';
-              } else {
-                echo 'class="home"><a href="https://' . $_SERVER['HTTP_HOST'] . '/index.php">';
-              } ?><i class="fa fa-home"></i></a></li>
+<li @if (Request::is('content')) class="active" @endif>
+  <a href="/content?id=2"><i class="fa fa-user-secret"></i> <span class="nav-label">Despre noi</span></a>
+</li>
 
-          <li <?php if ($page == "despre") {
-                echo 'class="active"';
-              } ?>><a href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/despre-noi.php">Despre noi</a></li>
+<li> @if (Request::is('for_companies'))" @endif>
+  <a href="/blog_list"><i class="fa fa-newspaper-o"></i> <span class="nav-label">Blog</span></a>
+</li>
 
-          <li <?php if ($page == "cursuri") {
-                echo 'class="active"';
-              } ?>><a href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/cursuri.php">Cursuri pentru tine </a>
-            <ul class="dropdown">
-              <?php
-              $sql_cursuri = "SELECT main_classes.* FROM `main_classes`
-JOIN `classes`
-ON `classes`.`main_class_id`=`main_classes`.`id`
-WHERE main_classes.`is_active`=1";
-
-              if (isset($academiaTestariiTrainerProvider)) {
-                $sql_cursuri .= " AND main_classes.trainer_provider_id = $academiaTestariiTrainerProvider";
-              }
-
-              $sql_cursuri .= " AND classes.is_active = 1
-GROUP by main_classes.id
-ORDER BY `main_classes`.`order` ASC";
-              $query_cursuri = mysqli_query($link, $sql_cursuri);
-              while ($row_cursuri = mysqli_fetch_assoc($query_cursuri)) {
-              ?>
-                <li><a href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/curs/<?php echo $row_cursuri['id'] . '/' . strtolower(str_replace(" ", "-", iconv('utf-8', 'ascii//TRANSLIT', $row_cursuri['title']))) ?>">
-                    <?php echo ($row_cursuri['is_new'] == 0) ? (str_replace("<br />", "", $row_cursuri['title'])) : ("<strong>NOU!</strong> " . str_replace("<br />", "", $row_cursuri['title'])); ?></a>
-                </li>
-              <?php } ?>
-            </ul>
-          </li>
-
-          <li <?php if ($page == "firme") {
-                echo 'class="active"';
-              } ?>><a href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/pentru-companii.php">Servicii pentru Companii</a></li>
-
-          <li <?php if ($page == "blog") {
-                echo 'class="active"';
-              } ?>><a href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/blog.php">Blog</a></li>
-
-          <li <?php if ($page == "contact") {
-                echo 'class="active"';
-              } ?>><a href="https://<?php echo $_SERVER['HTTP_HOST'] ?>/contact.php">Contact</a></li>
-
-        </ul>
-      </nav>
-    </div>
-  </div>
-</div>
+<li> @if (Request::is('contact'))" @endif>
+  <a href="/contact"><i class="fa fa-newspaper-o"></i> <span class="nav-label">Contact</span></a>
+</li>
