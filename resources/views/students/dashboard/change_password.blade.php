85dc60d964e1dc333f8455bd51bb8a1555d9b1c9
Marian Dumitru <d.mariandev@gmail.com>
2021-01-18 21:08:57 +0200

    change password include cleaneup. I named button in order to differenttiate requests as we have multiple forms in a single page

--- resources/views/students/dashboard/change_password.blade.php
+++ resources/views/students/dashboard/change_password.blade.php
@@ -1,34 +1,21 @@
-<div id="sectiuneaParola" class="panel-collapse collapse"
-     role="tabpanel" aria-labelledby="headingPreferinte"
+<div id="sectiuneaParola" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingPreferinte"
      aria-expanded="false">
     <div class="panel-body">
-        <form id="schimbaparola" name="schimbaparola"
-              class="reservation-form" method="post"
-              action="/student_dashboard" novalidate="novalidate"
+        <form id="password" name="password" class="reservation-form" method="POST" action="/student_dashboard" novalidate="novalidate"
               autocomplete="off">
+            @csrf
+            <input type="hidden" value="{{ $student->getId() }}" name='studentId'>
             <div class="row">
                 <div class="col-sm-12">
-                    <div class="form-group mb-30"><label>Schimbă parola
-                            <small>(maxim 12 caractere)</small></label>
-                        <input maxlength="12"
-                               placeholder="Seteaza parola" id="parola"
-                               name="parola" required=""
-                               class="form-control" aria-required="true"
-                               type="password" value="">
+                    <div class="form-group mb-30">
+                        <label for="password">Schimbă parola <small>(maxim 12 caractere)</small></label>
+                        <input id="password" name="password" class="form-control" type="password">
                     </div>
                 </div>
                 <div class="col-sm-12">
                     <div class="form-group mb-0 mt-0">
-                        <input name="form_botcheck" class="form-control"
-                               value="" type="hidden">
-                        <input name="action" class="form-control"
-                               value="schimbaparola" type="hidden">
-                        <input name="cursant" class="form-control"
-                               value="{{ $student->getId() }}"
-                               type="hidden">
-                        <button type="submit"
-                                class="btn btn-colored btn-block btn-theme-colored2 text-white btn-lg btn-flat"
-                               >Schimbă
+                        <button type="submit" class="btn btn-colored btn-block btn-theme-colored2 text-white btn-lg btn-flat"
+                               name="changePassword">Schimbă
                         </button>
                     </div>
                 </div>
