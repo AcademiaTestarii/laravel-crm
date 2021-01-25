343660eb0065674b39ee593886fc585f515bab33
laura <liancu@adoreme.com>
2021-01-25 17:57:29 +0200

    fixed UP-144

--- app/Http/Controllers/ClassesController.php
+++ app/Http/Controllers/ClassesController.php
@@ -108,14 +108,14 @@ class ClassesController extends Controller
                 }
 
                 if ($request->get('start')) {
-                    if ($class->getRegistrationStartDate()->format("Y-m-d") <= (new  Carbon($request->get('start')))->format("Y-m-d")
+                    if ($class->getRegistrationStartDate()->format("Y-m-d") < (new  Carbon($request->get('start')))->format("Y-m-d")
                     ) {
                         $mainClass->classes->forget($key);
                     }
                 }
 
                 if ($request->get('end')) {
-                    if ($class->getRegistrationEndDate()->format("Y-m-d") >= (new  Carbon($request->get('end')))->format("Y-m-d")
+                    if ($class->getRegistrationEndDate()->format("Y-m-d") > (new  Carbon($request->get('end')))->format("Y-m-d")
                     ) {
                         $mainClass->classes->forget($key);
                     }
